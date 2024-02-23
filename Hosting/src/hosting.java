793620a520ba1be75f57058c7a1accb0d90e38df
Estefanía Sánchez <estefi2000ms2@gmail.com>
2024-02-23 11:36:00 -0500

    v1

---
+++ Hosting/src/hosting.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class hosting {
+    private JPanel hostingp;
+    private JTextField nombreF;
+    private JTextField pasatiempoF;
+    private JTextField descripcionF;
+    private JButton ingresarButton;
+}
